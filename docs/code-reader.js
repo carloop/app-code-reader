ba6bf08a1d5992aea8746b842af2d99ab8ccba59
Julien Vanier <jvanier@gmail.com>
2016-10-16 16:29:32 -0400

    Working front end

--- docs/code-reader.js
+++ docs/code-reader.js
@@ -1,9 +1,10 @@
 var particle = new Particle();
 var token;
 var device;
+var eventStream;
 var $app = $("#app");
 
-var codeUrl = "https://raw.githubusercontent.com/carloop/app-code-reader/master/src/";
+var codeUrl = "https://raw.githubusercontent.com/carloop/app-code-reader/master/src";
 var appFiles = [
   "app-code-reader.cpp",
   "dtc.cpp",
@@ -108,7 +109,14 @@ function selectDeviceForm(force) {
 function mainUI() {
   $app.html(templates.mainUI());
 
-  var $flashButton = $("#flash-button").on('click', flashApp);
+  var $flashButton = $("#flash-button");
+  var $readButton = $("#read-codes");
+  var $clearButton = $("#clear-codes");
+  var $logoutButton = $("#logout-button");
+  $flashButton.on('click', flashApp);
+  $readButton.on('click', readCodes);
+  $clearButton.on('click', clearCodes);
+  $logoutButton.on('click', logout);
 }
 
 function timeoutPromise(ms) {
@@ -120,6 +128,7 @@ function timeoutPromise(ms) {
 }
 
 function flashApp() {
+  clearConsole();
   log("Start flashing...");
   var files = {};
 
@@ -145,7 +154,9 @@ function flashApp() {
     var body = data.body;
 
     if (body.ok) {
-      log("Done flashing!");
+      setTimeout(function () {
+        log("Done flashing!");
+      }, 2000);
     } else {
       error("Error during flash.");
       error(body.errors.join("\n"));
@@ -164,6 +175,64 @@ function flashApp() {
   });
 }
 
+function readCodes() {
+  setupEventStream()
+  .then(function () {
+
+    clearConsole();
+    log("Reading codes...");
+
+    var callPromise = particle.callFunction({
+      deviceId: device,
+      name: 'readCodes',
+      argument: '',
+      auth: token
+    });
+
+    // The result will be an event published by the device called codes/result
+
+    // Add timeout to function call
+    Promise.race([callPromise, timeoutPromise(10000)])
+    .catch(function (err) {
+      if (err.message == "Timeout") {
+        error("Timeout. Is your device connected to the Internet (breathing cyan)?");
+        return;
+      }
+      console.error(err);
+      error("Error while reading codes. Try flashing the app to your Carloop.");
+    });
+  });
+}
+
+function clearCodes() {
+  setupEventStream()
+  .then(function () {
+
+    clearConsole();
+    log("Clearing codes...");
+
+    var callPromise = particle.callFunction({
+      deviceId: device,
+      name: 'clearCodes',
+      argument: '',
+      auth: token
+    });
+
+    // The result will be an event published by the device called codes/cleared
+
+    // Add timeout to function call
+    Promise.race([callPromise, timeoutPromise(10000)])
+    .catch(function (err) {
+      if (err.message == "Timeout") {
+        error("Timeout. Is your device connected to the Internet (breathing cyan)?");
+        return;
+      }
+      console.error(err);
+      error("Error while clearing codes. Try flashing the app to your Carloop.");
+    });
+  });
+}
+
 function log(message) {
   printToConsole(message, 'info');
 }
@@ -172,14 +241,100 @@ function error(message) {
   printToConsole(message, 'error');
 }
 
-function printToConsole(message, type) {
+function printToConsole(message, type, rawHtml) {
   var $el = $('<div class="' + type + '"/>');
-  $el.text(message);
+  if (rawHtml) {
+    $el.html(message);
+  } else {
+    $el.text(message);
+  }
   $("#console").append($el);
+}
+
+function clearConsole() {
+  $("#console").html('');
 }
 
 function showError() {
   $app.html(templates.error());
+}
+
+function setupEventStream() {
+  if (!eventStream) {
+    return particle.getEventStream({
+      deviceId: device,
+      auth: token
+    })
+    .then(function (stream) {
+      eventStream = stream;
+
+      eventStream.on('codes/error', deviceError);
+      eventStream.on('codes/result', deviceCodes);
+      eventStream.on('codes/cleared', deviceCleared);
+    });
+  } else {
+    return Promise.resolve();
+  }
+}
+
+function deviceError(event) {
+  error("Carloop is online but is it connected to a car with the ignition on?");
+}
+
+function deviceCodes(event) {
+  try {
+    var codes = parseCodes(event.data);
+    displayCodes(codes);
+  } catch (err) {
+    console.error(err);
+    error("Error while processing codes. Blame Julien for this bug");
+  }
+}
+
+function parseCodes(encodedStr) {
+  var types = {
+    s: "stored",
+    p: "pending",
+    c: "cleared",
+  };
+
+  var codes = {};
+
+  encodedStr.split(",").forEach(function (encoded) {
+    var type = types[encoded[encoded.length - 1]];
+    var code = encoded.slice(0, encoded.length - 1);
+
+    codes[type] = codes[type] || [];
+    codes[type].push(code);
+  });
+
+  return codes;
+}
+
+function displayCodes(codes) {
+  var types = Object.keys(codes);
+  if (types.length == 0) {
+    info("No codes. Awesome!");
+  }
+  
+  types.forEach(function (type) {
+    var codesOfType = codes[type];
+    log(codesOfType.length + " " + type + " code" + (codesOfType.length > 1 ? "s" : "") + ":");
+    codesOfType.forEach(function (code) {
+      var html = '<a target="_blank" href="https://www.google.com/search?q=obdii+code+' + code + '">' + code + '</a> (click for more info)';
+      printToConsole(html, 'info', true);
+    });
+  });
+}
+
+function deviceCleared(event) {
+  log("Codes cleared!");
+}
+
+function logout() {
+  setToken('');
+  setDevice('');
+  window.location.reload();
 }
 
 
